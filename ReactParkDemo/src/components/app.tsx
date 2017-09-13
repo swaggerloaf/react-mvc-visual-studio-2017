c46fd4164b83eab38ac8f69773f5319491a7a994
Dewysockie <C16112@cfs.canon.com>
2017-09-13 16:09:58 -0400

    updates

---
+++ ReactParkDemo/src/components/app.tsx
@@ -0,0 +1,11 @@
+﻿import * as React from "react";
+
+export interface AppProps { name: string; }
+
+// 'HelloProps' describes the shape of props.
+// State is never set so we use the 'undefined' type.
+export class App extends React.Component<AppProps, undefined> {
+    render() {
+        return <h1>Let's go for a {this.props.name} react spin!</h1>
+    }
+}
---
+++ ReactParkDemo/src/index.tsx
@@ -0,0 +1,9 @@
+﻿import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { App } from "./components/app";
+
+ReactDOM.render(
+    <App name="quick" />,
+    document.getElementById("root")
+);
